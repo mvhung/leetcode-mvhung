03aa3b13c7dfdd9ab23c1c8192d06be27432f23f
mvhung <maihung1372002@gmail.com>
2024-09-18 19:43:19 +0700

    [18092024] 179. Largest number

--- leetcode/easy/884. Uncommon Words from Two Sentences.cs
+++ leetcode/easy/884. Uncommon Words from Two Sentences.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace leetcode.easy
 {
-    internal class _884
+
+    public class _884
     {
+        public string[] UncommonFromSentences(string s1, string s2)
+        {
+
+            var split1 = s1.Split(' ');
+            var split2 = s2.Split(' ');
+            var list1 = new List<string>();
+            var list2 = new List<string>();
+            foreach (var split in split1)
+            {
+                if (!list1.Contains(split))
+                {
+                    list1.Add(split);
+                    list2.Add(split);
+                }
+                else if (list1.Contains(split) && list2.Contains(split))
+                {
+                    list2.Remove(split);
+                }
+            }
+
+            foreach (var split in split2)
+            {
+                if (!list1.Contains(split))
+                {
+                    list1.Add(split);
+                    list2.Add(split);
+                }
+                else if (list1.Contains(split) && list2.Contains(split))
+                {
+                    list2.Remove(split);
+                }
+            }
+
+            return list2.ToArray();
+        }
     }
 }
---
+++ leetcode/medium/179. Largest Number.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.medium
+{
+    public class _179
+    {
+
+        public string LargestNumber(int[] nums)
+        {
+            string[] strs = nums.Select(x => x.ToString() + ).ToArray();
+
+            return "";
+        }
+
+        public class StringComparer : Comparer<string>
+        {
+            public override int Compare(string? x, string? y)
+            {
+                int xy = int.Parse(x + y);
+                int yx = int.Parse(y + x);
+                if (xy > yx)
+                {
+                    return 1;
+                }
+                else if (xy == yx) { return 0; }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+    }
+}
